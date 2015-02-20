194020538fab75a89c54e84bf7f51c3f48fa429f
me_twinklescat <lajatomary@gmail.com>
2015-02-20 15:27:16 +0800

    Added the necessary js files for the creation of tabs, animation of menu, etc

---
+++ js/swipe.js
@@ -0,0 +1,7 @@
+(function($) {
+  var options = {
+      $AutoPlay: false,                                   
+      $DragOrientation: 1
+    };
+   var jssor_slider1 = new $JssorSlider$("ingredients-container", options);
+})(jQuery)
