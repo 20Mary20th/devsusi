cf7d227c73adf96bb87770158341c73177feb2fb
me_twinklescat <lajatomary@gmail.com>
2015-02-23 14:51:19 +0800

    Remove the previous contents due to conflicts of the new template provided for the navigation

--- js/swipe.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-  var options = {
-      $AutoPlay: false,                                   
-      $DragOrientation: 1
-    };
-   var jssor_slider1 = new $JssorSlider$("ingredients-container", options);
-})(jQuery)
