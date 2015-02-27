fd0f21cc9ca6d01f8f833746d6550a82f3c832d9
me_twinklescat <lajatomary@gmail.com>
2015-02-27 09:11:37 +0800

    Revising the Splash Page:
      --> When user opens the application, The developers logo will be the only one to be displayed only. No more showing of the actual app logo according to QA.

--- js/splash.js
+++ js/splash.js
@@ -3,25 +3,24 @@
   var win = $(window).width;
   var wrapper = $('div.wrapper-logo-splash');
   var logo1 = $('img#logo-gems');
-  var logo2 = $('img#logo-susi');
-  var logos = $('img').length;
-  var ctr = -1;
+  // var logos = $('img').length;
+  // var ctr = -1;
   var show = 500;
-  var hide = 1500;
-  var showdelay = 2500;
-  var interval = 5300;
+  var hide = 1000;
+  var showdelay = 2000;
+  var interval = 3000;
       wrapper.css({
        'width' : win,
       });
   
-  var timeout = setInterval(function() {
-  	   ctr += 1;
-  	   if (ctr != logos) {
-         $('img').eq(ctr).fadeIn(show).delay(showdelay).fadeOut(hide);
-     }
-  	   else {
-  	   	clearTimeout(timeout);
-  	   	window.location.replace("login.html");
-  	   } 
-     }, interval); 
+  var set = {
+      displaylogo : function() {
+        setTimeout(function() {
+        logo1.fadeIn(500).delay(showdelay).fadeOut(hide, function(){
+          window.location = "login.html";
+        });
+        }, interval);
+      }  
+  }
+   set.displaylogo();
 })(jQuery)
