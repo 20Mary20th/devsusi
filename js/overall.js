5a36b1a25446b5a7bb7b84f70ad5a839fd2dc3e6
me_twinklescat <lajatomary@gmail.com>
2015-03-02 16:39:02 +0800

    Category Pages and Checkout Page: Cleaning code base and remove unnecessary css and js files. Recompile the provided js to [overall] to remove inline js on the same HTML DOM

--- js/overall.js
+++ js/overall.js
@@ -32,36 +32,36 @@
       });
     },
 
-    viewGMap : function() {
-     var mapCanvas =  document.getElementById('map-canvas');
-     var map = new google.maps.Map(mapCanvas);
+    // viewGMap : function() {
+    //  var mapCanvas =  document.getElementById('map-canvas');
+    //  var map = new google.maps.Map(mapCanvas);
 
-     var mapOptions = {
-       center: new google.maps.LatLng(14.5401828, 121.0415443,14),
-       zoom: 8,
-       mapTypeId: google.maps.MapTypeId.ROADMAP
-     }
-     var map = new google.maps.Map(mapCanvas, mapOptions);
-    },
+    //  var mapOptions = {
+    //    center: new google.maps.LatLng(14.5401828, 121.0415443,14),
+    //    zoom: 8,
+    //    mapTypeId: google.maps.MapTypeId.ROADMAP
+    //  }
+    //  var map = new google.maps.Map(mapCanvas, mapOptions);
+    // },
 
     cartCompute : function() {
       var price = 0 ;
       var del = $(".price-delivery").data('delivery');
-        $(".product-container").each(function(){
+        $(".product-container").each(function() {
           quantity = $(this).find(".quantity").attr('value');
           price  +=  ($(this).find(".price").data('price') * quantity );
 
           $(this).find(".quantity-price").text($(this).find(".price").data('price') * quantity);
         });
-        $('.price-total span').text(price);
-        $('.total-amount-price span').text(price + del);
+          $('.price-total span').text(price);
+          $('.total-amount-price span').text(price + del);
       
-        if (price == 0) {
-          $('.price-delivery').text('P 0');
-          $('.total-amount-price span').text('P 0');
+          if (price == 0) {
+            $('.price-delivery').text('P 0');
+            $('.total-amount-price span').text('P 0');
 
-          $('.mobile-nav').fadeIn(1000);
-          $("button.close").click(function() {
+            $('.mobile-nav').fadeIn(1000);
+            $("button.close").click(function() {
             $(".mobile-nav").fadeOut(1000);
         });
         // var r = confirm("Cart Empty Would You like to go to Menu page?");
@@ -79,6 +79,38 @@
           }
       });
     },
+
+    dropDown: function(el, multiple) {
+      var Accordion = function(el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+
+    var links = this.el.find('.link');
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+  }
+
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el;
+      $this = $(this),
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+    };
+  } 
+    },
+
+    // promptMsg : function() {
+    //    $("button.main-nav").click(function() {
+    //         $(".mobile-nav").fadeIn(1000);
+    //     });
+    //     $("button.close").click(function() {
+    //         $(".mobile-nav").fadeOut(1000);
+    //     });
+    // },
     
   };
 
@@ -86,5 +118,7 @@
   setjs.aboutNav();
   setjs.cartCompute();
   setjs.viewGMap();
+  // setjs.dropDown($('#accordion'), false);
+  // setjs.promptMsg();
   google.maps.event.addDomListener(window, 'load', setjs.viewGMap);
 })(jQuery)
