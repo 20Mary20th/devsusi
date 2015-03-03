431366db5725c0137a31c7a9ff7423441ff9b297
me_twinklescat <lajatomary@gmail.com>
2015-03-03 11:34:48 +0800

    Page Entree: Cleaning code base and add additional asset [social-media.js] and [page-entree.css] for replacing the inline css on the same HTML

---
+++ js/social-media.js
@@ -0,0 +1,31 @@
+(function($) {
+
+//   (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=807063136013927&version=v2.0";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+
+  // window.fbAsyncInit = function() {
+  //   FB.init({
+  //     appId      : '807063136013927',
+  //     xfbml      : true,
+  //     version    : 'v2.2'
+  //   });
+  // };
+
+  // (function(d, s, id){
+  //    var js, fjs = d.getElementsByTagName(s)[0];
+  //    if (d.getElementById(id)) {return;}
+  //    js = d.createElement(s); js.id = id;
+  //    js.src = "//connect.facebook.net/en_US/sdk.js";
+  //    fjs.parentNode.insertBefore(js, fjs);
+  //  }(document, 'script', 'facebook-jssdk'));
+
+  // FB.ui({
+  // method: 'share',
+  // href: 'https://developers.facebook.com/docs/',
+  // }, function(response){});
+})(jQuery)
