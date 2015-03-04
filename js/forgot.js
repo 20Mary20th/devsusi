b3944e6810667f82d9b02188f2a2093b6bee03b9
me_twinklescat <lajatomary@gmail.com>
2015-03-04 13:51:14 +0800

    Forgot your Password Form: When user clicks on the send email, the Message will only appear instead. Edit the asset[forgot.js] to hide the other elements

--- js/forgot.js
+++ js/forgot.js
@@ -5,6 +5,8 @@
   receive.on('click', function() {
   	$('img#forgot-back').css({'display' : 'none'});
   	$('div.forgot-logo1').addClass('small-12 columns');
+
+  	$('div.wrapper-forgot').css({'display' : 'none'});
     $('div.container-msg1').load('forgot-msg.html', 'div.modal-forgot');
   });
 })(jQuery);
