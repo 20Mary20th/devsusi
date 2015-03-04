9eeff3ac50f91623c4f9a1daf6c61a5a396818eb
me_twinklescat <lajatomary@gmail.com>
2015-03-04 13:45:16 +0800

    Checkout Page: Replace asset[overall.js] to [checkout.js] and fix layout for the Forgot your Password Form

---
+++ js/forgot.js
@@ -0,0 +1,10 @@
+(function($){
+	// console.log('it work');
+  var receive = $('a.send-email1');
+
+  receive.on('click', function() {
+  	$('img#forgot-back').css({'display' : 'none'});
+  	$('div.forgot-logo1').addClass('small-12 columns');
+    $('div.container-msg1').load('forgot-msg.html', 'div.modal-forgot');
+  });
+})(jQuery);
