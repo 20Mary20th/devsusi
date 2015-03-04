3c83d0a2c874419e856f32bf2354d04ddaa3b129
me_twinklescat <lajatomary@gmail.com>
2015-03-04 13:53:17 +0800

    Cleaning code base based on proper coding standard

--- js/forgot.js
+++ js/forgot.js
@@ -1,12 +1,9 @@
 (function($){
-	// console.log('it work');
   var receive = $('a.send-email1');
-
-  receive.on('click', function() {
-  	$('img#forgot-back').css({'display' : 'none'});
-  	$('div.forgot-logo1').addClass('small-12 columns');
-
-  	$('div.wrapper-forgot').css({'display' : 'none'});
-    $('div.container-msg1').load('forgot-msg.html', 'div.modal-forgot');
-  });
+      receive.on('click', function() {
+  	    $('img#forgot-back').css({'display' : 'none'});
+  	    $('div.forgot-logo1').addClass('small-12 columns');
+        $('div.wrapper-forgot').css({'display' : 'none'});
+        $('div.container-msg1').load('forgot-msg.html', 'div.modal-forgot');
+    });
 })(jQuery);
